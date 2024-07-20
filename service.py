fb0d28718b81d70239cdecc0b1ed34aff8ead41c
Noel Soares <noelsoares408@gmail.com>
2024-07-20 19:48:09 +0530

    service.py file updated

--- service.py
+++ service.py
@@ -1,26 +1,20 @@
 from fastapi import FastAPI
 from joblib import load
 from pydantic import BaseModel
+import pandas as pd
 
 app = FastAPI()
 
 class PredictionInput(BaseModel):
     # Define the input parameters required for making predictions
     vendor_id: float
+    pickup_datetime: float
     passenger_count: float
     pickup_longitude: float
     pickup_latitude: float
     dropoff_longitude: float
     dropoff_latitude: float
     store_and_fwd_flag: float
-    distance_haversine: float
-    distance_dummy_manhattan: float
-    direction: float
-    pickup_weekday: float
-    pickup_hour: float
-    pickup_minute: float
-    pickup_dt: float
-    pickup_week_hour: float
 
 
 # Load the pre-trained RandomForest model
@@ -34,22 +28,19 @@ def home():
 @app.post("/predict")
 def predict(input_data: PredictionInput):
     # Extract features from input_data and make predictions using the loaded model
-    features = [input_data.vendor_id,
-                input_data.passenger_count,   
-                input_data.pickup_longitude,  
-                input_data.pickup_latitude,   
-                input_data.dropoff_longitude, 
-                input_data.dropoff_latitude,  
-                input_data.store_and_fwd_flag,     
-                input_data.distance_haversine,
-                input_data.distance_dummy_manhattan,
-                input_data.direction,
-                input_data.pickup_weekday,
-                input_data.pickup_hour,
-                input_data.pickup_minute,
-                input_data.pickup_dt,
-                input_data.pickup_week_hour
-                ]
+    features = {
+        'vendor_id': input_data.vendor_id,
+        'pickup_datetime': input_data.pickup_datetime,
+        'passenger_count': input_data.passenger_count,
+        'pickup_longitude': input_data.pickup_longitude,
+        'pickup_latitude': input_data.pickup_latitude,
+        'dropoff_longitude': input_data.dropoff_longitude,
+        'dropoff_latitude': input_data.dropoff_latitude,
+        'store_and_fwd_flag': input_data.store_and_fwd_flag,
+    }
+    
+    features = pd.DataFrame(features, index=[0])
+    features = feature_build(features, 'prod')
     prediction = model.predict([features])[0].item()
     # Return the prediction
     return {"prediction": prediction}
